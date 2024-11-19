8855bdcf95bd0b68619c8fdb0a90c15a8ea29b36
Syed Muhammad Armghan Ahmad <168146316+SyedArmghanAhmad@users.noreply.github.com>
2024-11-19 16:48:48 +0500

    Initial commit

---
+++ Email Sender/email_sender.py
@@ -0,0 +1,23 @@
+from email.message import EmailMessage
+from email.policy import SMTP
+import ssl
+import smtplib
+
+email_sender ='mb15lhr@gmail.com'
+email_password = 'uuxp xget mxlx vogu'   # use your own password
+email_receiver = 'popal42125@cashbn.com' # use any temp email.
+
+subject="hey this email is sent by python :D cool right?"
+body="""hello this email is being sent by a code. a python program. just a test as a mini project :D"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtplib:
+    smtplib.login(email_sender, email_password)
+    smtplib.send_message(em)
+    print("Email sent successfully")~
